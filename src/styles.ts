4bfdb3aad25673cb2bf3f8bb8bddac56251cf1ac
leao <lucasleaolima@gmail.com>
2024-05-07 15:09:42 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    h1 {
+        color: green;
+    }
+`
+export default EstiloGlobal
